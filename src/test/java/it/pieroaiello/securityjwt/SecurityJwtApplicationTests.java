10634235664546ee658599958a583a6d6c861d38
Piero Aiello <piero.aiello96@icloud.com>
2022-10-06 11:55:54 +0200

    first commit

---
+++ src/test/java/it/pieroaiello/securityjwt/SecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package it.pieroaiello.securityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
